e6b9e66910197c7f2b071decc704219283d651a7
Richard Lyders <richard@lyders.com>
2025-01-13 20:39:06 -0600

    fix only show resident RAM for MacOS since that is the only RAM value I'm getting that makes sense so far

--- rust/src/utils/system.rs
+++ rust/src/utils/system.rs
@@ -1,21 +1,19 @@
-use std::env::consts::ARCH;
 use std::env::consts::OS;
 use std::fmt::{self, Debug};
 use sysconf::pagesize;
-use sysinfo::{Pid, System};
+use sysinfo::System;
 use rustc_version_runtime::version;
 
-use crate::filters::rustc_version;
-
 use super::system_windows_n_macos::get_os_mem_stats_windows_n_macos;
-
-// #[cfg(target_os = "windows")]
 
 const BYTES_PER_KB: usize = 1024;
 
 pub fn print_mem_stats(title: String) {
     #[cfg(target_os = "windows")]
-    super::system_windows_n_macos::print_mem_stats_windows(title);
+    super::system_windows_n_macos::print_mem_stats_windows_n_macos(title);
+
+    #[cfg(target_os = "macos")]
+    super::system_windows_n_macos::print_mem_stats_windows_n_macos(title);
 
     #[cfg(target_os = "linux")]
     super::system_linux::print_mem_stats_linux(title);
@@ -85,20 +83,6 @@ pub fn get_os_info() -> String {
         os = "MacOS"
     }
     format!("rust v{} on {} {} {}", version().to_string(), os, get_system_info(), get_os_mem_stats())
-    // hostStat, _ := host.Info()
-    // cpuStat, _ := cpu.Info()
-    // vmStat, _ := mem.VirtualMemory()
-    // diskStat, _ := disk.Usage("\\") // If you're in Unix change this "\\" for "/"
-
-    // info = new(SysInfo)
-
-    // // info.Hostname = hostStat.Hostname
-    // info.Platform = hostStat.Platform;
-    // info.CPU = cpuStat[0].ModelName;
-    // info.RAM = vmStat.Total / 1024 / 1024;
-    // info.Disk = diskStat.Total / 1024 / 1024
-
-    // fmt.Printf("OS: %s: ARCH: %s %s %s GB RAM\n", std::env::consts::OS, std::env::consts::ARCH, info.Platform, info.CPU, bytes_str(info.RAM*1024, None));
 }
 
 pub fn get_system_info() -> String {
@@ -112,12 +96,4 @@ pub fn get_system_info() -> String {
     System::name().unwrap_or("?".to_string()), 
     System::kernel_version().unwrap_or("?".to_string()),
     sys.cpus().len())
-    // // Display system information:
-    // println!("System name:             {:?}", System::name());
-    // println!("System kernel version:   {:?}", System::kernel_version());
-    // println!("System OS version:       {:?}", System::os_version());
-    // println!("System host name:        {:?}", System::host_name());
-
-    // // Number of CPUs:
-    // println!("NB CPUs: {}", sys.cpus().len());
 }
--- rust/src/utils/system_windows_n_macos.rs
+++ rust/src/utils/system_windows_n_macos.rs
@@ -1,5 +1,6 @@
-use sysinfo::{Pid, System};
 use memory_stats::memory_stats;
+use std::process;
+use sysinfo::System;
 
 // #[cfg(target_os = "windows")]
 pub fn get_os_mem_stats_windows_n_macos() -> String {
@@ -18,81 +19,37 @@ pub fn get_os_mem_stats_windows_n_macos() -> String {
 }
 
 // #[cfg(target_os = "windows")]
-pub fn print_mem_stats_windows(title: String) {
+pub fn print_mem_stats_windows_n_macos(title: String) {
     use crate::utils::system::bytes_str;
 
     let mut sys: System = System::new_all();
 
-    // First we update all information of our `System` struct.
     sys.refresh_all();
-   
-    // println!("=> system:");
-    // RAM and swap information:
+
     if let Some(usage) = memory_stats() {
+        #[cfg(target_os = "windows")]
+        let resident_ram = bytes_str(usage.physical_mem, None);
+        #[cfg(target_os = "windows")]
+        let ram = bytes_str(usage.virtual_mem, None);
+
+        #[cfg(target_os = "macos")]
+        let ram = "?"; // on MacOS, I can not find correlation between usage.physical_mem here and what appears as "VSZ" in ps... makes no sense to me so I'm not showing on MacOS
+        #[cfg(target_os = "macos")]
+        let resident_ram: String = bytes_str(usage.physical_mem, None);
+
         println!(
-            "{}: {} of {} RAM",
+            "# [PID {:6}] {:-18}: RAM: {:8} (resident: {:8} share: {:8} code: {:8} data: {:8})",
+            process::id(),
             title,
-            bytes_str(usage.virtual_mem, None),
-            bytes_str(usage.physical_mem, None),
-            );
+            ram,
+            resident_ram,
+            "?", // bytes_str(s.share, None),
+            "?", // bytes_str(s.text, None),
+            "?", // bytes_str(s.data, None)
+        );
     } else {
         println!("Couldn't get the current memory usage :(");
     }
-
-    // println!(
-    //     "total swap  : {} bytes",
-    //     bytes_str(sys.total_swap() as usize, None)
-    // );
-    // println!(
-    //     "used swap   : {} bytes",
-    //     bytes_str(sys.used_swap() as usize, None)
-    // );
-
-    // Display processes ID, name na disk usage:
-    // for (pid, process) in sys.processes() {
-    //     println!("[{pid}] {:?} {:?}", process.name(), process.disk_usage());
-    // }
-
-    // // We display all disks' information:
-    // println!("=> disks:");
-    // let disks = Disks::new_with_refreshed_list();
-    // for disk in &disks {
-    //     println!("{disk:?}");
-    // }
-
-    // // Network interfaces name, total data received and total data transmitted:
-    // let networks = Networks::new_with_refreshed_list();
-    // println!("=> networks:");
-    // for (interface_name, data) in &networks {
-    //     println!(
-    //         "{interface_name}: {} B (down) / {} B (up)",
-    //         data.total_received(),
-    //         data.total_transmitted(),
-    //     );
-    //     // If you want the amount of data received/transmitted since last call
-    //     // to `Networks::refresh`, use `received`/`transmitted`.
-    // }
-
-    // // Components temperature:
-    // let components = Components::new_with_refreshed_list();
-    // println!("=> components:");
-    // for component in &components {
-    //     println!("{component:?}");
-    // }
-
-    // match r {
-    //     Ok(s) => println!(
-    //         "# [PID {:6}] {:-18}: RAM: {:8} (resident: {:8} share: {:8} code: {:8} data: {:8})",
-    //         process::id(),
-    //         title,
-    //         pages_str(s.size),
-    //         pages_str(s.resident),
-    //         pages_str(s.share),
-    //         pages_str(s.text),
-    //         pages_str(s.data)
-    //     ),
-    //     Err(e) => eprintln!("failed to get results from statm: {}", e),
-    // }
 }
 
 pub fn print_system_cpu() {
@@ -110,21 +67,3 @@ pub fn print_system_cpu() {
     // Number of CPUs:
     println!("NB CPUs: {}", sys.cpus().len());
 }
-
-// fn get_process_memory(pid: Pid) -> Result<u64, sysinfo::Error> {
-
-//     let mut system = System::new();
-
-//     system.refresh_process(pid)?;
-
-//     let process = system.process(pid).unwrap();
-
-//     Ok(process.memory())
-
-// }
-
-// Usage example:
-
-// let process_memory = get_process_memory(1234).unwrap(); // Get memory usage of process with PID 1234
-
-// println!("Process memory usage: {} bytes", process_memory);
